15946ab0947986c9c1babf83f341e973f36fb5a3
caiobaraujo <caiobrega@gmail.com>
2022-01-03 11:25:30 -0300

    api/index.js update

--- http/api/index.js
+++ http/api/index.js
@@ -1,8 +1,9 @@
 const http = require('http');
-const data = require('./urls.json');
 const URL = require('url');
 const fs = require('fs');
 const path = require('path');
+
+const data = require('./urls.json');
 
 function writeFile(cb) {
   fs.writeFile(
@@ -10,6 +11,7 @@ function writeFile(cb) {
     JSON.stringify(data, null, 2),
     (err) => {
       if (err) throw err;
+
       cb(JSON.stringify({ message: 'ok' }));
     }
   );
@@ -20,10 +22,11 @@ http
     const { name, url, del } = URL.parse(req.url, true).query;
 
     res.writeHead(200, {
-      'Access-Control-Allow-Origin': '*', // garante o acesso a API para qualquer origem
+      'Access-Control-Allow-Origin': '*',
     });
 
-    if (!name || !url) res.end(JSON.stringify(data));
+    // all resources
+    if (!name || !url) return res.end(JSON.stringify(data));
 
     if (del) {
       data.urls = data.urls.filter((item) => String(item.url) !== String(url));
@@ -34,4 +37,4 @@ http
 
     return writeFile((message) => res.end(message));
   })
-  .listen(3000, () => console.log('API is running'));
+  .listen(3000, () => console.log('Api is running'));
